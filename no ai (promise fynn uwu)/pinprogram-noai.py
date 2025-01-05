fd0b3fe8f5e9c61e8acf82d940e007a73c8ca872
Alex <aaj1234offical@gmail.com>
2025-01-05 19:09:30 +0800

    yes

--- no ai (promise fynn uwu)/pinprogram-noai.py
+++ no ai (promise fynn uwu)/pinprogram-noai.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 def mainmenu():
 
@@ -9,16 +10,29 @@ def mainmenu():
     print("Made by: alex")
     print(" ")
     print("Please select an option:")
-    print("1. Set Pin")
-    print("2. Enter Pin")
-    print("3. Exit")
+    print("1. Enter Pin")
+    print("2. Exit")
     print(" ")
     choice = input("> ")
+    
+    if choice == '1':
+        print("Enter your pin")
+        pininput = input("> ")
+        if pininput == pin:
+            print("Pin correct!")
+            time.sleep(10)
+            clear()
+            exit()
+        else:
+            print("skill issue buddy")
+            time.sleep(10)
+            clear()
+            mainmenu()
+    
+    if choice == '2':
+        clear()
+        exit()
 
+print("Before we enter the program, please enter your preferred pin")
+pin = input("> ")
 mainmenu()
-
-if choice == '1':
-    print("Enter a new pin!")
-    pin = input("Enter your new pin!")
-    print("Alright!")
-    mainmenu()
