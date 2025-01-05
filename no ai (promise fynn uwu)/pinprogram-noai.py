58b494a8469d7d13cee8265dbcb154e7918e49d3
Alex <aaj1234offical@gmail.com>
2025-01-05 18:29:11 +0800

    no ai

---
+++ no ai (promise fynn uwu)/pinprogram-noai.py
@@ -0,0 +1,11 @@
+print("Welcome to my random pin program!")
+print("Made by: alex")
+print(" ")
+print("Please select an option:")
+print("1. Set Pin")
+print("2. Enter Pin")
+print("3. Exit")
+print(" ")
+choice = input("> ")
+
+if c
