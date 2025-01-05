2bed1d05cc75c579258639bbdd4ba4cffbf74333
Alex <aaj1234offical@gmail.com>
2025-01-05 19:09:20 +0800

    new ting

--- no ai (promise fynn uwu)/pinprogram-noai.py
+++ no ai (promise fynn uwu)/pinprogram-noai.py
@@ -1,11 +1,24 @@
-print("Welcome to my random pin program!")
-print("Made by: alex")
-print(" ")
-print("Please select an option:")
-print("1. Set Pin")
-print("2. Enter Pin")
-print("3. Exit")
-print(" ")
-choice = input("> ")
+import os
 
-if c
+def mainmenu():
+
+    clear = lambda: os.system('cls')
+    clear()
+
+    print("Welcome to my random pin program!")
+    print("Made by: alex")
+    print(" ")
+    print("Please select an option:")
+    print("1. Set Pin")
+    print("2. Enter Pin")
+    print("3. Exit")
+    print(" ")
+    choice = input("> ")
+
+mainmenu()
+
+if choice == '1':
+    print("Enter a new pin!")
+    pin = input("Enter your new pin!")
+    print("Alright!")
+    mainmenu()
